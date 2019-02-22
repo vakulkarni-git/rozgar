f14ad0d1d2d61fdb014d61989535844e1a12c6c2
Sudhanshu Shekhar <sudhanshu@git.edu>
2019-02-22 16:01:24 +0530

    Initial changes

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -13,25 +13,19 @@
         <span>{{ worker.comment }}</span>
       </div>
       <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-dirty">
-          <label for="stitching" class="mdl-textfield__label" style="font-size:large;">{{ worker.req1 }}</label>
-          <select class="mdl-selectfield__select" id="stitching" name="stitching" style="margin-top:10px;">
-            <option value="option1">Yes</option>
-            <option selected="selected" value="option2">No</option>
-          </select>
+          <label for="skill" class="mdl-textfield__label" style="font-size:large;">
+          just
+          </label>          
       </div>
       <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-dirty">
-          <label for="management" class="mdl-textfield__label" style="font-size:large;">{{ worker.req2 }}</label>
-          <select class="mdl-selectfield__select" id="management" name="management" style="margin-top:10px;">
-            <option value="option1">Yes</option>
-            <option selected="selected" value="option2">No</option>
-          </select>
+          <label for="skill" class="mdl-textfield__label" style="font-size:large;">
+          just
+          </label>
       </div>
       <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-dirty">
-          <label for="skill" class="mdl-textfield__label" style="font-size:large;">{{ worker.req3 }}</label>
-          <select class="mdl-selectfield__select" id="skill" name="skill" style="margin-top:10px;">
-            <option value="option1">Yes</option>
-            <option selected="selected" value="option2">No</option>
-          </select>
+          <label for="skill" class="mdl-textfield__label" style="font-size:large;">
+          just
+          </label>
       </div>
       <div class="actions">
         <router-link class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" to="/post">
